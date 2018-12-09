7b862df58b7d89cb57b39a2a27c92eaf3d295b4c
Gabriel Uribe <gabriel.uribe.022@gmail.com>
2018-12-09 13:37:25 -0300

    se agrego boton agregar

---
+++ src/containers/AddTodo.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity
+} from "react-native";
+
+import { Ionicons } from '@expo/vector-icons'
+import { connect } from 'react-redux'
+
+
+class AddTodo extends Component {
+
+    state = {
+        text: ''
+    }
+
+    addTodo = (text) => {
+        //redux store 
+        this.props.dispatch({ type: 'ADD_TODO', text })
+        this.setState({ text: '' })
+    }
+
+    render() {
+        return (
+            <View style={{ flexDirection: 'row', marginHorizontal: 20 }}>
+                <TextInput
+                    onChangeText={(text) => this.setState({ text })}
+                    value={this.state.text}
+                    placeholder="Ingrese elemento a la lista"
+                    style={{ borderWidth: 1, borderColor: '#f2f2e1', backgroundColor: '#eaeaea', height: 50, flex: 1, padding: 5 }}
+                />
+                <TouchableOpacity onPress={() => this.addTodo(this.state.text)}>
+                    <View style={{ height: 50, backgroundColor: '#eaeaea', alignItems: 'center', justifyContent: 'center' }}>
+                        <Ionicons name="md-add" size={30} style={{ color: '#de9595', padding: 10 }} />
+                    </View>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+export default connect()(AddTodo);
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
